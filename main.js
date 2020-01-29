32e6066125f3a8294b75106c726cc79bc42cc6b8
maaush <mauribenitez95@gmail.com>
2020-01-28 22:57:46 -0300

    reformat code in order to use js guidelines

--- main.js
+++ main.js
@@ -3,35 +3,75 @@ function computerSelection() {
   let computerChoice = Math.floor(Math.random() * 3) + 1;
   let result;
 
-  switch (computerDecision) {
+  switch (computerChoice) {
     case 1:
-      result = "rock";
+      result = 'rock';
       return result;
     case 2:
-      result = "paper";
+      result = 'paper';
       return result;
     case 3:
-      result = "scissors";
+      result = 'scissors';
       return result;
   }
 }
 
 let playerSelection = () => {
   let playerChoice = prompt(
-    " Please, enter your choice between rock, paper and scissors: ",
-    "rock"
+     'Please, enter your choice between rock, paper and scissors:' ,
+    'rock'
   );
   playerChoice = playerChoice.toLowerCase();
 
   if (
-    playerChoice === "rock" ||
-    playerChoice === "paper" ||
-    playerChoice === "scissors"
+    playerChoice === 'rock' ||
+    playerChoice === 'paper' ||
+    playerChoice === 'scissors'
   ) {
     return playerChoice;
   } else {
-    return "Wrong input, you must select either rock, paper or scissors.";
+    return 'Wrong input, you must select either rock, paper or scissors.';
   }
 };
 
+
+let play = (playerChoice, computerChoice) => {
+    
+    let playerDecision = playerChoice();
+    let computerDecision = computerChoice();
+
+};
+
+let finalDecision = (playerChoice, computerChoice) =>  {
+
+    let playerWin = false;
+    let playerLose = false;
+    let tie = false;
+
+    switch (playerChoice) {
+
+        case "rock":
+            if (computerChoice === playerChoice) {
+
+                finalDecision = console.log('It\'s a tie between you and the computer.');
+                tie = false;
+
+                return tie;
+            } else if (computerChoice == "paper") {
+
+                finalDecision = console.log('You Lose! Paper beats Rock');
+                playerLose = true;
+
+                return playerLose;
+            } else {
+
+                console.log('You Win! Rock beats Scissors!.');
+                playerWin = true;
+                
+                return playerWin;
+            }
+            
+            break;
+    }
+}
 
