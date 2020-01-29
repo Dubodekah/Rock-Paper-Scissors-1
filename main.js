0ae72f2fca5fe79407e29fb44a454b6b57985855
maaush <mauribenitez95@gmail.com>
2020-01-29 00:04:53 -0300

    Finish the project

--- main.js
+++ main.js
@@ -1,77 +1,136 @@
 function computerSelection() {
-
   let computerChoice = Math.floor(Math.random() * 3) + 1;
   let result;
 
   switch (computerChoice) {
     case 1:
-      result = 'rock';
+      result = "rock";
       return result;
     case 2:
-      result = 'paper';
+      result = "paper";
       return result;
     case 3:
-      result = 'scissors';
+      result = "scissors";
       return result;
   }
 }
 
 let playerSelection = () => {
   let playerChoice = prompt(
-     'Please, enter your choice between rock, paper and scissors:' ,
-    'rock'
+    "Please, enter your choice between rock, paper and scissors:",
+    "rock"
   );
   playerChoice = playerChoice.toLowerCase();
 
   if (
-    playerChoice === 'rock' ||
-    playerChoice === 'paper' ||
-    playerChoice === 'scissors'
+    playerChoice === "rock" ||
+    playerChoice === "paper" ||
+    playerChoice === "scissors"
   ) {
     return playerChoice;
   } else {
-    return 'Wrong input, you must select either rock, paper or scissors.';
+    return "Wrong input, you must select either rock, paper or scissors.";
   }
 };
 
+let match = (playerChoice, computerChoice) => {
+  let playerScore = false;
 
-let play = (playerChoice, computerChoice) => {
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === playerChoice) {
+        console.log("Rock, It's a tie between you and the computer.");
+
+        return "tie";
+      } else if (computerChoice === "paper") {
+        console.log("You Lose! Paper beats Rock");
+        playerScore = false;
+
+        return playerScore;
+      } else {
+        console.log("You Win! Rock beats Scissors!.");
+        playerScore = true;
+
+        return playerScore;
+      }
+
+    case "paper":
+      if (computerChoice === playerChoice) {
+        console.log("Paper!, It's a tie between you and the computer.");
+
+        return "tie";
+      } else if (computerChoice === "scissors") {
+        console.log("You Lose! Scissors beats Paper!");
+        playerScore = false;
+
+        return playerScore;
+      } else {
+        console.log("You Win! Paper beats Rock!.");
+        playerScore = true;
+
+        return playerScore;
+      }
+
+    case "scissors":
+      if (computerChoice === playerChoice) {
+        console.log("Scissors!, It's a tie between you and the computer.");
     
-    let playerDecision = playerChoice();
-    let computerDecision = computerChoice();
+        return "tie";
+      } else if (computerChoice === "rock") {
+        console.log("You Lose! Rock beats Scissors!");
+        playerScore = false;
 
+        return playerScore;
+      } else {
+        console.log("You Win! Scissors beats Paper!.");
+        playerScore = true;
+
+        return playerScore;
+      }
+  }
 };
 
-let finalDecision = (playerChoice, computerChoice) =>  {
+function game() {
 
-    let playerWin = false;
-    let playerLose = false;
-    let tie = false;
+    let result = false;
+    let playerWin = 0; 
+    let computerWin = 0;
+    let tie = 0;
 
-    switch (playerChoice) {
+    for (let i = 1; i <= 5; i++) {
 
-        case "rock":
-            if (computerChoice === playerChoice) {
+        let roundMessage = `Round ${i}`;
+        let player = playerSelection();
+        let computer = computerSelection();
+        console.log(roundMessage);
 
-                finalDecision = console.log('It\'s a tie between you and the computer.');
-                tie = false;
+        result = match(player, computer);
 
-                return tie;
-            } else if (computerChoice == "paper") {
+        if (result == "tie") {
 
-                finalDecision = console.log('You Lose! Paper beats Rock');
-                playerLose = true;
+            tie++;
+        } else if (result == true) {
 
-                return playerLose;
-            } else {
+            playerWin++;
+        } else {
 
-                console.log('You Win! Rock beats Scissors!.');
-                playerWin = true;
-                
-                return playerWin;
-            }
-            
-            break;
+            computerWin++;
+        }
     }
-}
 
+    console.log("End of the best of five match! results: ");
+    console.log(`Player: ${playerWin} points.`);
+    console.log(`Computer: ${computerWin} points.`);
+    console.log(`Ties: ${tie}`);
+
+    if (playerWin > computerWin) {
+
+        console.log(`Player wins! by a difference in points of: ${playerWin - computerWin} points.`);
+    } else if (computerWin > playerWin) {
+
+        console.log(`Computer wins! by a difference in points of: ${computerWin - playerWin} points!`);
+    } else {
+
+        console.log(`Best of five ended in a tie, Player: ${playerWin}, Computer:${computerWin}`);
+    }
+};
